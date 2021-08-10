d8a4287a7a0eabab41858f86bf7d002232dacb58
shoumaa1216 <shoumaa1216@gmail.com>
2021-08-10 14:50:39 +0900

    b

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
 {
 public function index(Post $post)
 {
-    return view('index')->with(['posts' => $post->getPaginate()]);
+    return view('index')->with(['posts' => $post->getPaginateByLimit()]);
 }
 
 public function show(Post $post)
